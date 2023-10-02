08775809a87b2748d80e9c372a4ee3c21b4dad6c
tom <tom.simul@gmail.com>
2023-10-02 09:46:03 +0200

    Update constraints

--- src/main/java/org/example/GLIAAirlines.java
+++ src/main/java/org/example/GLIAAirlines.java
@@ -2,13 +2,9 @@ package org.example;
 
 import org.chocosolver.solver.Model;
 import org.chocosolver.solver.Solution;
-import org.chocosolver.solver.constraints.Constraint;
 import org.chocosolver.solver.variables.IntVar;
-
-import java.util.Arrays;
-
-import static org.chocosolver.solver.search.strategy.Search.minDomLBSearch;
-import static org.chocosolver.util.tools.ArrayUtils.append;
+import org.chocosolver.solver.variables.RealVar;
+import org.chocosolver.solver.variables.Variable;
 
 public class GLIAAirlines {
 
@@ -32,9 +28,11 @@ public class GLIAAirlines {
 
 		if(allSolutions){
 			while (solution != null){
-				System.out.println(solution.retrieveIntVars(false));
+				prettyPrintSol();
 				solution = model.getSolver().findSolution();
 			}
+		} else {
+			prettyPrintSol();
 		}
 
 		model.getSolver().printStatistics();
@@ -49,39 +47,43 @@ public class GLIAAirlines {
 		dividers = new IntVar[inst.nb_dividers];
 
 		dividers[0] = model.intVar("D"+1, 0);
-		dividers[inst.nb_dividers-1] = model.intVar("D"+inst.nb_dividers, inst.capacity);
 		for(int i = 1; i < inst.nb_dividers-1; i++){
 			dividers[i] = model.intVar("D"+(i+1), 1, inst.capacity-1);
 		}
+		dividers[inst.nb_dividers-1] = model.intVar("D"+inst.nb_dividers, inst.capacity);
 
 		IntVar[] distances = new IntVar[(inst.nb_dividers*inst.nb_dividers-inst.nb_dividers)/2];
 
 		int count = 0;
 		for (int i = 0; i < inst.nb_dividers; i++) {
 			for (int j = i+1; j < inst.nb_dividers; j++) {
-				distances[count] = dividers[j].sub(dividers[i]).intVar();
+				distances[count] = model.intVar("D"+(i+1) + "_sub_" + "D"+(j+1), 0, inst.capacity);
 				count++;
 			}
 		}
 
 		// CONSTRAINTS
-		model.arithm(dividers[1].sub(dividers[0]).intVar(), ">=", 2).post();
+		model.arithm(dividers[1], "-", dividers[0], ">=", 2).post();
+		count = 0;
+		for (int i = 0; i < inst.nb_dividers; i++) {
+			for (int j = i+1; j < inst.nb_dividers; j++) {
+				model.arithm(dividers[j], "-", dividers[i], "=", distances[count]).post();
+				count++;
+			}
+		}
 		model.allDifferent(distances).post();
-		model.among(model.intVar(inst.exits.length-1), dividers, inst.exits).post();
+		model.among(model.intVar("", inst.exits.length-1), dividers, inst.exits).post();
+
 		model.increasing(dividers, 1).post();
-
-		/*for (Constraint c: model.getCstrs()) {
-			System.out.println(c);
-		}*/
 	}
 
-
-
-
-	public void configureSearch() {
-		model.getSolver().setSearch(minDomLBSearch(append(dividers)));
-
+	private void prettyPrintSol(){
+		String sol = "";
+		for (Variable var : model.getVars()) {
+			//if (var.toString().contains("D"))
+				sol += var + "; ";
+		}
+		System.out.println(sol);
 	}
-
 
 }
